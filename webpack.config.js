83d01fce0ffff3ada6d8a2a652bc90325f240d62
HL <nunavutaccountant2@gmail.com>
2016-06-15 13:40:56 +0800

    React loader working. Outputting a single test component.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,16 @@
+var path = require('path');
+
 module.exports = {
-    entry:  './app',
-    output: {
-        path:     'builds',
-        filename: 'bundle.js',
-    },
+  entry:  './src',
+  output: {
+      path:     'builds',
+      filename: 'bundle.js',
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'src')
+    }]
+  }  
 };
