c8165538416ce8b35cddd5defa5a59e580f1a911
HL <nunavutaccountant2@gmail.com>
2016-06-15 16:53:16 +0800

    HMR, React hot loader working.

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
-const config = require('./webpack.config');
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config');
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
-  historyApiFallback: true,
-}).listen(3000, 'localhost', err => {
+  historyApiFallback: true
+}).listen(3000, 'localhost', function (err, result) {
   if (err) {
     return console.log(err);
   }
 
-  return console.log('Listening at http://localhost:3000/');
+  console.log('Listening at http://localhost:3000/');
 });
--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <h1>111111Hello, world.</h1>
+      <h1>123123Hello, world.</h1>
     );
   }
 }
