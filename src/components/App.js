bb29a6c6e17ca10e30aa31e111bf66f3c3e71ae2
HL <hi.heyse@gmail.com>
2016-06-16 19:20:34 +0800

    Refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-// import styles from '../style.css';
+import styles from '../style.css';
 
 import Header from './Header/index';
 
 export default class App extends Component {  // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <div>
+      <div className={styles.app}>
         <Header />
       </div>
     );
