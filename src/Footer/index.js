267f59ca92248bb07f2907ee1e923e670161dd3e
HL <hi.heyse@gmail.com>
2016-06-17 17:40:19 +0800

    Footer skeleton

---
+++ src/Footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+/* eslint-disable react/prefer-stateless-function */
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <div>Footer</div>
+    );
+  }
+}
