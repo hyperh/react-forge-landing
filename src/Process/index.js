746c2d7c7ce6abc5b163f6e32dd9eaa06f170295
HL <hi.heyse@gmail.com>
2016-06-17 12:34:54 +0800

    Centering image

--- src/Process/index.js
+++ src/Process/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import styles from './styles.css';
 
 /* eslint-disable react/prefer-stateless-function */
 export default class Process extends React.Component {
   render() {
     const imgSrc = require('../assets/process-horiz.png'); // eslint-disable-line
     return (
-      <div>
-        Process
+      <div className={styles.container}>
         <img src={imgSrc} alt="The ReactForge Process." />
       </div>
     );
