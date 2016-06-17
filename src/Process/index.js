ef337e98eeab695725879046fe995383b9ac553b
HL <hi.heyse@gmail.com>
2016-06-17 15:22:18 +0800

    Split process image into 3 parts. Will manage with CSS.

--- src/Process/index.js
+++ src/Process/index.js
@@ -4,11 +4,16 @@ import styles from './styles.css';
 /* eslint-disable react/prefer-stateless-function */
 export default class Process extends React.Component {
   render() {
-    const imgSrc = require('../assets/process-horiz.png'); // eslint-disable-line
+    const laptop = require('../assets/laptop.png'); // eslint-disable-line global-require
+    const fileTypes = require('../assets/file-types.png'); // eslint-disable-line global-require
+    const code = require('../assets/code.png'); // eslint-disable-line global-require
+
     return (
       <div className={styles.container}>
-        <div>
-          <img src={imgSrc} alt="The ReactForge Process." className={styles['img-responsive']} />
+        <div className={`${styles.process}`}>
+          <img src={laptop} alt="Laptop" className={styles['img-responsive']} />
+          <img src={fileTypes} alt="File Types" className={styles['img-responsive']} />
+          <img src={code} alt="Code" className={styles['img-responsive']} />
         </div>
       </div>
     );
