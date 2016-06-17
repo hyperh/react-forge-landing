c3c2dc809d3d975cc1fcaaaa59e4a3b4b3d06a7f
HL <hi.heyse@gmail.com>
2016-06-17 12:11:41 +0800

    Process skeleton

---
+++ src/Process/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+/* eslint-disable react/prefer-stateless-function */
+export default class Process extends React.Component {
+  render() {
+    return (
+      <div>Process 2</div>
+    );
+  }
+}
