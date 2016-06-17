7c4342edbfeb20f58ffa3d7b20047ab2bd0e7724
HL <hi.heyse@gmail.com>
2016-06-17 18:52:35 +0800

    Fixed image paths not working

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import styles from '../styles.css';
 
 import Header from './Header/index';
+import Process from './Process/index';
 
 export default class App extends Component {  // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <div className={styles.app}>
         <Header />
+        <Process />
       </div>
     );
   }
--- src/components/Process/index.js
+++ src/components/Process/index.js
@@ -4,10 +4,10 @@ import styles from './styles.css';
 /* eslint-disable react/prefer-stateless-function */
 export default class Process extends React.Component {
   render() {
-    const laptop = require('../assets/laptop.png'); // eslint-disable-line global-require
-    const fileTypes = require('../assets/file-types.png'); // eslint-disable-line global-require
-    const arrow = require('../assets/arrow-right.png'); // eslint-disable-line global-require
-    const code = require('../assets/code.png'); // eslint-disable-line global-require
+    const laptop = require('../../assets/laptop.png'); // eslint-disable-line global-require
+    const fileTypes = require('../../assets/file-types.png'); // eslint-disable-line global-require
+    const arrow = require('../../assets/arrow-right.png'); // eslint-disable-line global-require
+    const code = require('../../assets/code.png'); // eslint-disable-line global-require
 
     return (
       <div className={styles.container}>
