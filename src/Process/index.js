3b454a7016b4be91462ee8f1e8875c4f94ce6e9e
HL <hi.heyse@gmail.com>
2016-06-17 12:18:16 +0800

    Showing process pic

--- src/Process/index.js
+++ src/Process/index.js
@@ -3,8 +3,12 @@ import React from 'react';
 /* eslint-disable react/prefer-stateless-function */
 export default class Process extends React.Component {
   render() {
+    const imgSrc = require('../assets/process-horiz.png'); // eslint-disable-line
     return (
-      <div>Process 2</div>
+      <div>
+        Process
+        <img src={imgSrc} alt="The ReactForge Process." />
+      </div>
     );
   }
 }
