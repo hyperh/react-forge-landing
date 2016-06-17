72dfbfdb7126e4fc23ec03cae90a6b848931d618
HL <hi.heyse@gmail.com>
2016-06-17 16:53:43 +0800

    Added step number and text

--- src/Process/index.js
+++ src/Process/index.js
@@ -12,12 +12,27 @@ export default class Process extends React.Component {
     return (
       <div className={styles.container}>
         <div className={styles.process}>
-          <img src={laptop} alt="Laptop" className={styles.laptop} />
+
+          <div className={styles.stepContainer}>
+            <div className={styles.stepText}>
+              <div className={styles.numberCircle}>1</div>
+              <div>Submit your design</div>
+            </div>
+            <img src={laptop} alt="Laptop" className={styles.laptop} />
+          </div>
+
           <div className={styles.centerContainer}>
             <img src={fileTypes} alt="File Types" className={styles.fileTypes} />
             <img src={arrow} alt="File Types" className={styles.arrow} />
           </div>
-          <img src={code} alt="Code" className={styles.code} />
+
+          <div className={styles.stepContainer}>
+            <div className={styles.stepText}>
+              <div className={styles.numberCircle}>2</div>
+              <div>Receive code in 5 days</div>
+            </div>
+            <img src={code} alt="Code" className={styles.code} />
+          </div>
         </div>
       </div>
     );
