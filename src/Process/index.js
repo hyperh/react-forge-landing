a23064dc7950d8b57fa68c18afef57dd9925bfe4
HL <hi.heyse@gmail.com>
2016-06-17 16:36:32 +0800

    Added arrow. Process now responsive.

--- src/Process/index.js
+++ src/Process/index.js
@@ -6,13 +6,17 @@ export default class Process extends React.Component {
   render() {
     const laptop = require('../assets/laptop.png'); // eslint-disable-line global-require
     const fileTypes = require('../assets/file-types.png'); // eslint-disable-line global-require
+    const arrow = require('../assets/arrow-right.png'); // eslint-disable-line global-require
     const code = require('../assets/code.png'); // eslint-disable-line global-require
 
     return (
       <div className={styles.container}>
-        <div className={`${styles.process}`}>
+        <div className={styles.process}>
           <img src={laptop} alt="Laptop" className={styles.laptop} />
-          <img src={fileTypes} alt="File Types" className={styles.fileTypes} />
+          <div className={styles.filesAndArrow}>
+            <img src={fileTypes} alt="File Types" className={styles.fileTypes} />
+            <img src={arrow} alt="File Types" className={styles.arrow} />
+          </div>
           <img src={code} alt="Code" className={styles.code} />
         </div>
       </div>
