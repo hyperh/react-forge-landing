00446dcac9c58ffbdd878f24ffc11190335448c4
HL <hi.heyse@gmail.com>
2016-06-17 17:45:44 +0800

    Simple footer done

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import styles from './styles.css';
+
 /* eslint-disable react/prefer-stateless-function */
 export default class Footer extends React.Component {
   render() {
     return (
-      <div>Footer</div>
+      <div className={styles.container}>
+        <div>Artisinally crafted by Adrian and friends.</div>
+        <div>Copyright ReactForge 2016.</div>
+      </div>
     );
   }
 }
