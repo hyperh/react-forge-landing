ad0adf2911f39aac20bb0c907047ecc5dd23a664
HL <hi.heyse@gmail.com>
2016-06-21 16:42:48 +0800

    Added gh-pages build script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+webpack
+
+cp index.html ./dist/index.html
+cp CNAME ./dist/CNAME
+
+git add -f dist && git commit -m 'new build'
+git subtree split --prefix dist -b gh-pages
+git push -f origin gh-pages:gh-pages
+git branch -D gh-pages
