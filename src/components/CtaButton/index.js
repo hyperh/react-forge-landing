3eb49b31c3c55cdb5f6d7819ffd0972665dd0b4d
Adrian Li <li.adrianmc@gmail.com>
2016-06-21 16:12:13 +0800

    new button for typeform

--- src/components/CtaButton/index.js
+++ src/components/CtaButton/index.js
@@ -3,11 +3,34 @@ import styles from './styles.css';
 
 /*  eslint-disable react/prefer-stateless-function */
 class CtaButton extends Component {
+  /* eslint-disable */
+  componentDidMount() {
+    var qs, js, q, s, d = document,
+      gi = d.getElementById,
+      ce = d.createElement,
+      gt = d.getElementsByTagName,
+      id = 'typef_orm',
+      b = 'https://s3-eu-west-1.amazonaws.com/share.typeform.com/';
+    if (!gi.call(d, id)) {
+      js = ce.call(d, 'script');
+      js.id = id;
+      js.src = b + 'share.js';
+      q = gt.call(d, 'script')[0];
+      q.parentNode.insertBefore(js, q);
+    }
+    /* eslint-enable */
+  }
+
   render() {
     return (
-      <button className={`${styles.btn}`}>
+      <a
+        className={`typeform-share link ${styles.btn}`}
+        href="https://adrianmcli.typeform.com/to/Kq866Q"
+        data-mode="2"
+        target="_blank"
+      >
         Submit Your Design!
-      </button>
+      </a>
     );
   }
 }
