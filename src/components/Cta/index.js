84a47610a3f22bbecc2cda09a98ac833b8e29233
Adrian Li <li.adrianmc@gmail.com>
2016-09-25 21:38:57 -0400

    put site into construction mode

--- src/components/Cta/index.js
+++ src/components/Cta/index.js
@@ -9,7 +9,7 @@ class Cta extends Component {
       <div className={styles.cta}>
         <p className={styles.description}>
           Get a page of mockups converted to <b>React</b>
-          <br /> components for just <b>$97!</b>
+          <br /> components for as low as <b>$97!</b>
         </p>
         <CtaButton />
       </div>
