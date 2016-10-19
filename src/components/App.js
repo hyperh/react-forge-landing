4c10daa21dea67b7efdc0f53a61fcdbb08f5835c
HL <hi.heyse@gmail.com>
2016-10-19 15:24:15 -0400

    Revert "put site into construction mode"
    This reverts commit 32d6b51a73002e11d5428ba02ce9e2b57af0e58e.

--- src/components/App.js
+++ src/components/App.js
@@ -2,29 +2,21 @@ import React, { Component } from 'react';
 import styles from '../styles.css';
 
 import Header from './Header/';
-// import Process from './Process/';
+import Process from './Process/';
 import Footer from './Footer/';
-// import Cta from './Cta/';
-// import PreviousWorksSection from './PreviousWorksSection/';
-// import BenefitsSection from './BenefitsSection/';
-import Construction from './Construction/';
+import Cta from './Cta/';
+import PreviousWorksSection from './PreviousWorksSection/';
+import BenefitsSection from './BenefitsSection/';
 
 export default class App extends Component {  // eslint-disable-line react/prefer-stateless-function
   render() {
-    // return (
-    //   <div className={styles.app}>
-    //     <Header />
-    //     <Process />
-    //     <BenefitsSection />
-    //     <PreviousWorksSection />
-    //     <Cta />
-    //     <Footer />
-    //   </div>
-    // );
     return (
       <div className={styles.app}>
         <Header />
-        <Construction />
+        <Process />
+        <BenefitsSection />
+        <PreviousWorksSection />
+        <Cta />
         <Footer />
       </div>
     );
